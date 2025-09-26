61e23056f50c2997a6e856882bba05081b32f87f
Ahmed Nabil <ahmednab93@gmail.com>
2025-09-26 17:58:00 +0300

    feat: refactor HeaderSection to use HookWidget and extract welcome message

--- lib/presentation/screens/dashboard/widgets/header_section.dart
+++ lib/presentation/screens/dashboard/widgets/header_section.dart
@@ -1,25 +1,21 @@
+import 'package:expense_tracker_lite/core/constants.dart';
 import 'package:expense_tracker_lite/core/enums/expense_filter.dart';
 import 'package:expense_tracker_lite/presentation/screens/dashboard/bloc/expenses_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'balance_card.dart';
 
-class HeaderSection extends StatefulWidget {
+class HeaderSection extends HookWidget {
   const HeaderSection({super.key});
-
-  @override
-  State<HeaderSection> createState() => _HeaderSectionState();
-}
-
-class _HeaderSectionState extends State<HeaderSection> {
-  ExpenseFilter _selectedFilter = ExpenseFilter.monthly;
 
   @override
   Widget build(BuildContext context) {
     final isListExpanded = context.select(
       (ExpensesBloc bloc) => bloc.state.isListExpanded,
     );
+    final selectedFilter = useState(ExpenseFilter.monthly);
     return SizedBox(
       height: !isListExpanded
           ? MediaQuery.of(context).size.height * 0.43
@@ -43,37 +39,7 @@ class _HeaderSectionState extends State<HeaderSection> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      children: const [
-                        CircleAvatar(
-                          backgroundColor: Colors.white,
-                          child: Icon(Icons.person, color: Colors.blue),
-                        ),
-                        SizedBox(width: 12),
-                        Column(
-                          spacing: 5,
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Good Morning",
-                              style: TextStyle(
-                                color: Colors.white70,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Text(
-                              "Shihab Rahman",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                    _WelcomeMessage(),
                     Container(
                       padding: const EdgeInsets.symmetric(horizontal: 8),
                       decoration: BoxDecoration(
@@ -81,7 +47,7 @@ class _HeaderSectionState extends State<HeaderSection> {
                         borderRadius: BorderRadius.circular(8),
                       ),
                       child: DropdownButton<ExpenseFilter>(
-                        value: _selectedFilter,
+                        value: selectedFilter.value,
                         padding: EdgeInsets.all(5),
                         isDense: true,
                         dropdownColor: Colors.white,
@@ -102,9 +68,9 @@ class _HeaderSectionState extends State<HeaderSection> {
                         }).toList(),
                         onChanged: (value) {
                           if (value != null) {
-                            setState(() => _selectedFilter = value);
+                            selectedFilter.value = value;
                             context.read<ExpensesBloc>().add(
-                              ExpensesRefreshed(filter: _selectedFilter),
+                              ExpensesRefreshed(filter: selectedFilter.value),
                             );
                           }
                         },
@@ -115,20 +81,6 @@ class _HeaderSectionState extends State<HeaderSection> {
               ],
             ),
           ),
-          // Positioned(
-          //   left: 50,
-          //   child: Align(
-          //     alignment: Alignment.topCenter,
-          //     child: RotatedBox(
-          //       quarterTurns: 2,
-          //       child: Image.asset(
-          //         'assets/header_bg.png',
-          //         width: 200,
-          //         color: Colors.white.withValues(alpha: 0.1),
-          //       ),
-          //     ),
-          //   ),
-          // ),
           if (!isListExpanded)
             Positioned(
               top: MediaQuery.of(context).size.height * 0.17,
@@ -139,6 +91,45 @@ class _HeaderSectionState extends State<HeaderSection> {
             ),
         ],
       ),
+    );
+  }
+}
+
+class _WelcomeMessage extends StatelessWidget {
+  const _WelcomeMessage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Icon(Icons.person, color: Colors.blue),
+        ),
+        SizedBox(width: 12),
+        Column(
+          spacing: 5,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Good Morning",
+              style: TextStyle(
+                color: Colors.white70,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              kUserName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
